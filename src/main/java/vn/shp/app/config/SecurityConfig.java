d2dbacd8639b10b594e338c24940e75d1637c141
Nguyen Ha <nguyenha3535@gmail.com>
2018-02-08 07:57:09 +0700

    Update CTDT

--- src/main/java/vn/shp/app/config/SecurityConfig.java
+++ src/main/java/vn/shp/app/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().and()
                 .logout()
                 .permitAll()
-                .and().sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(true).sessionRegistry(sessionRegistry());
+                .and().sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(false).sessionRegistry(sessionRegistry());
         //passwordEncoder.
     }
 
